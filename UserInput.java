906f41538df68acbb3e48c0d44e194bc437f8cb1
Nimishan <nimishan.siv@gmail.com>
2023-12-23 11:17:24 +0530

    Update UserInput.java
    Remove the last Thank you message

--- UserInput.java
+++ UserInput.java
@@ -4,19 +4,16 @@ class UserInput{
 		Scanner s = new Scanner(System.in);
 		System.out.println("Enter your 1st number");
 		int a = s.nextInt();
-		
+
 		System.out.println("Enter your 2nd number");
 		int b = s.nextInt();	
 		
 		int quotient = a/b;
 		int remainder = a%b;
 		
-		
 		System.out.println("_________");
 		System.out.println("the Quotient is "+ quotient);
 		System.out.println(remainder);
-		System.out.println("=========");
-		System.out.println("Thank You!!!");
-	
+		System.out.println("=========");	
 	}
 }
